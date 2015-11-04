f67cf2a372d20422c7164d8f8eb38d384059ff7c
Ivaylo <ivailokirilov93@gmail.com>
2015-11-04 14:54:11 +0200

    Initial

---
+++ Haiku/Models/Dtos/UserRegisterDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Haiku.Models.Dtos
+{
+    public class UserRegisterDto
+    {
+        [Required]
+        [MinLength(4)]
+        public string Nickname { get; set; }
+
+        [Required]
+        [MinLength(30)]
+        [MaxLength(30)]
+        public string PublishToken { get; set; }
+    }
+}
