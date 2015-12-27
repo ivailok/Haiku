718bea46980bb26d17a423d756d3261fdb3b4925
Ivaylo Kirilov <ivailokirilov93@gmail.com>
2015-12-27 22:09:26 +0200

    added bootstrap-select

--- Haiku.Web/App_Start/BundleConfig.cs
+++ Haiku.Web/App_Start/BundleConfig.cs
@@ -21,10 +21,12 @@ namespace Haiku.Web
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootstrap-select.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-select.css",
                       "~/Content/site.css",
                       "~/Content/font-awesome-4.5.0/css/font-awesome.min.css"));
         }
---
+++ Haiku.Web/Scripts/app/directives/selectPicker.js
@@ -0,0 +1,10 @@
+﻿app.directive("selectPicker", ['$timeout', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            $timeout(function () {
+                element.selectpicker();
+            });
+        }
+    };
+}]);
