9c9ba7410f2f64d88171f71a52b1e9da4f96a2fe
Ivaylo Kirilov <ivailokirilov93@gmail.com>
2016-01-12 19:18:26 +0200

    Revert "authorization modal stays on error in public app"
    This reverts commit afc2f87b699cd3ea8fc38d966e3f46c1026406da.

--- Haiku.Web/Scripts/app/controllers/authorize-controller.js
+++ Haiku.Web/Scripts/app/controllers/authorize-controller.js
@@ -1,32 +1,19 @@
 ﻿app.controller("AuthorizeController", ['$scope', '$location', '$uibModalInstance', 'HaikusService', 'UsersService',
 function ($scope, $location, $uibModalInstance, haikusService, usersService) {
     
-    $scope.publishCode = '';
-
-    $scope.unauthorized = false;
-    $scope.responseMessage = '';
-
     $scope.authorize = function () {
         var hash = CryptoJS.SHA3($scope.publishCode, { outputLength: 512 });
 
         if ($scope.authorizeType === 'DeleteSingleHaiku') {
             haikusService.deleteHaiku($scope.haiku.author, $scope.haiku.id, hash.toString())
                 .then(function (httpResponse) {
-                    $uibModalInstance.close();
                     $scope.onDelete();
-                }, function (httpResponse) {
-                    $scope.unauthorized = true;
-                    $scope.responseMessage = httpResponse.data.message;
                 });
         }
         else if ($scope.authorizeType === 'DeleteAllHaikus') {
             usersService.deleteHaikus($scope.user.nickname, hash.toString())
                 .then(function (httpResponse) {
-                    $uibModalInstance.close();
                     $scope.onDelete();
-                }, function (httpResponse) {
-                    $scope.unauthorized = true;
-                    $scope.responseMessage = httpResponse.data.message;
                 });
         }
         else if ($scope.authorizeType === 'EditHaiku') {
@@ -35,13 +22,11 @@ function ($scope, $location, $uibModalInstance, haikusService, usersService) {
             };
             haikusService.modifyHaiku($scope.haiku.author, $scope.haiku.id, hash.toString(), data)
                 .then(function (httpResponse) {
-                    $uibModalInstance.close();
                     $scope.onModified();
-                }, function (httpResponse) {
-                    $scope.unauthorized = true;
-                    $scope.responseMessage = httpResponse.data.message;
                 });
         }
+        
+        $uibModalInstance.close();
     };
 
     $scope.cancel = function () {
